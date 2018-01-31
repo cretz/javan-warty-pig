7b6b8f62c1639d6ba7191384348ad7b09b38ff3d
Chad Retz <chad.retz@gmail.com>
2018-01-31 12:29:20 -0600

    Some params and encoder option for htmlsan example

--- examples/htmlsan/README.md
+++ examples/htmlsan/README.md
@@ -7,5 +7,9 @@ format. To run the example, execute the following from the repo root:
     path/to/gradle --no-daemon :examples:htmlsan:run
 
 This will run for 1 minute and store some cache files at `examples/build/tmp`. When executed a second time, it will pick
-up from where it left off. The example is set to output any parameter that causes an exception to be thrown, but the
-project is carefully written to not allow that to happen so it is unlikely any will occur.
+up from where it left off. The example is set to output any parameter that causes an exception to be thrown or leaves
+some HTML artifact, but the project is carefully written to not allow that to happen so it is unlikely any will occur.
+
+An extra couple of system-property-based parameters have been added to. If the `-Djwp.noPersist` parameter is present,
+persistence will not be used and will be in-memory only. If the `-Djwp.fuzzEncoder` is present, the
+[Java encoder project](https://github.com/OWASP/owasp-java-encoder) is fuzzed instead.
--- examples/htmlsan/src/main/java/jwp/examples/htmlsan/Main.java
+++ examples/htmlsan/src/main/java/jwp/examples/htmlsan/Main.java
@@ -4,6 +4,9 @@ import jwp.extras.AflDictionary;
 import jwp.extras.FilePersistence;
 import jwp.extras.TestWriter;
 import jwp.fuzz.*;
+import org.owasp.encoder.Encode;
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
 import org.owasp.html.examples.EbayPolicyExample;
 
 import java.nio.charset.StandardCharsets;
@@ -14,27 +17,56 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
 
 public class Main {
   public static void main(String[] args) throws Throwable {
     Files.createDirectories(Paths.get("build/tmp/"));
+
+    // Whether to use persistence
+    boolean persist = System.getProperty("jwp.noPersist") == null;
+
+    // Whether to run the encoder instead
+    boolean encoderInsteadOfSanitizer = System.getProperty("jwp.fuzzEncoder") != null;
+
     // Keep track of total counts and last new hash
     AtomicLong completeCounter = new AtomicLong();
     AtomicLong lastNewHashAtCounter = new AtomicLong();
-    // Keep track of our seen hashes in a file instead of memory (not to be confused with the
-    //  file-based hash cache we will give to the byte array generator)
-    FilePersistence.FileBasedHashCache seenHashes = new FilePersistence.FileBasedHashCache(
-        new FilePersistence.FileBasedHashCache.Config(
-            Paths.get("build/tmp/seen-hashes"), null, 30L
-        )
-    );
-    System.out.println("Reloaded " + seenHashes.backingSet.size() + " seen hashes");
+
+    // Keep track of our seen hashes (not to be confused with the hash cache we will give to the byte array generator)
+    ByteArrayParamGenerator.HashCache.SetBacked seenHashes;
+    // Creators for byte gen hash cache and input queue
+    Function<ByteArrayParamGenerator.Config, ByteArrayParamGenerator.HashCache> hashCacheCreator = null;
+    Function<ByteArrayParamGenerator.Config, ByteArrayParamGenerator.InputQueue> inputQueueCreator = null;
+
+    // Build persisted versions
+    if (persist) {
+      seenHashes = new FilePersistence.FileBasedHashCache(
+          new FilePersistence.FileBasedHashCache.Config(
+              Paths.get("build/tmp/seen-hashes"), null, 30L
+          )
+      );
+      System.out.println("Reloaded " + seenHashes.backingSet.size() + " seen hashes");
+      hashCacheCreator = config -> new FilePersistence.FileBasedHashCache(
+          new FilePersistence.FileBasedHashCache.Config(
+              Paths.get("build/tmp/byte-array-cache"), null, 50L
+          )
+      );
+      inputQueueCreator = config -> new FilePersistence.FileBasedInputQueue(
+          new FilePersistence.FileBasedInputQueue.Config(
+              config, Paths.get("build/tmp/byte-array-queue"), null, 50L
+          )
+      );
+    } else {
+      seenHashes = new ByteArrayParamGenerator.HashCache.SetBacked();
+    }
 
     // Create the fuzzer
-    System.out.println("Creating fuzzer");
+    System.out.println(String.format("Creating fuzzer %s persistence to fuzz the %s",
+        persist ? "with" : "without", encoderInsteadOfSanitizer ? "encoder" : "sanitizer"));
     Fuzzer fuzzer = new Fuzzer(Fuzzer.Config.builder().
         // Static parser method
-        method(Main.class.getDeclaredMethod("sanitize", String.class)).
+        method(Main.class.getDeclaredMethod(encoderInsteadOfSanitizer ? "encode" : "sanitize", String.class)).
 
         // The suggested param provider w/ the single string param generator
         params(new ParamProvider.Suggested(
@@ -45,17 +77,9 @@ public class Main {
                     ByteArrayParamGenerator.Config.builder().
                         // Initial value that succeeds
                         initialValues(Arrays.asList("<pre>test</pre>".getBytes())).
-                        // File-backed hash cache and input queue
-                        hashCacheCreator(config -> new FilePersistence.FileBasedHashCache(
-                            new FilePersistence.FileBasedHashCache.Config(
-                                Paths.get("build/tmp/byte-array-cache"), null, 50L
-                            )
-                        )).
-                        inputQueueCreator(config -> new FilePersistence.FileBasedInputQueue(
-                            new FilePersistence.FileBasedInputQueue.Config(
-                                config, Paths.get("build/tmp/byte-array-queue"), null, 50L
-                            )
-                        )).
+                        // Pre-created (maybe) hash cache and input queue
+                        hashCacheCreator(hashCacheCreator).
+                        inputQueueCreator(inputQueueCreator).
                         // Dictionary of existing terms
                         dictionary(AflDictionary.read(Main.class.getResource("html_tags.dict")).listOfBytes()).
                         // Build the complete config
@@ -67,16 +91,22 @@ public class Main {
         // Handler to print out unique paths
         onSubmit((config, fut) -> fut.thenApply(res -> {
           long count = completeCounter.incrementAndGet();
-          if (count % 1000 == 0) System.out.println("Completed " + count + " executions");
           int hash = BranchHit.Hasher.WITHOUT_HIT_COUNTS.hash(res.branchHits);
           // Only print output of failures (synchronized to prevent stdout overwrite)
           if (seenHashes.checkUniqueAndStore(hash)) {
             lastNewHashAtCounter.set(count);
-            if (res.exception != null) synchronized (Main.class) {
-              System.out.println("New path for param " + TestWriter.doubleQuotedString(res.params[0].toString()) +
-                  ", result: " + res.exception + ", hash: " + hash +
-                  " (after " + completeCounter + " total execs this run)");
+            if (res.exception != null || (res.result != null &&
+                (res.result.toString().indexOf('<') != -1 || res.result.toString().indexOf('>') != -1))) {
+              synchronized (Main.class) {
+                System.out.println("New path for param " + TestWriter.doubleQuotedString(res.params[0].toString()) +
+                    ", result: " + (res.exception == null ? res.result : res.exception) + ", hash: " + hash +
+                    " (after " + completeCounter + " total execs this run)");
+              }
             }
+          }
+          if (count % 10000 == 0) {
+            System.out.println("Completed " + count + " executions with " +
+                seenHashes.backingSet.size() + " unique paths");
           }
           return res;
         })).
@@ -95,7 +125,14 @@ public class Main {
         lastNewHashAtCounter + ")");
   }
 
+  private static final PolicyFactory allowNothingPolicy = new HtmlPolicyBuilder().toFactory();
+
   public static String sanitize(String str) {
-    return EbayPolicyExample.POLICY_DEFINITION.sanitize(str);
+    // Remove the comment section sometimes embedded
+    return allowNothingPolicy.sanitize(str).replace("<!-- -->", "");
+  }
+
+  public static String encode(String str) {
+    return Encode.forHtml(str);
   }
 }
